c1c271822cd747e16625e7e212795bac077afda2
Franck-olivier FRAGONARD <olivierfragonard@hotmail.com>
2024-01-14 12:39:39 +0100

    Add project files.

---
+++ GloboTicket.TicketManagement.Domain/Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿using GloboTicket.TicketManagement.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Domain.Entities
+{
+    public class Category : AuditableEntity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }=string.Empty;
+        public ICollection<Event>? Events {  get; set; }
+    }
+}
---
+++ GloboTicket.TicketManagement.Domain/Entities/Common/AuditableEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Domain.Entities.Common
+{
+    public class AuditableEntity
+    {
+        public string? CreatedBy { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public string? LastUpdatedBy { get; set; } = string.Empty;
+        public DateTime? LastUpdatedDate { get; private set; }
+    }
+}
